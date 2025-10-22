eb9032dd98616f41b32ebcf0560e022629ebeab2
DnielCC <123047063@upq.edu.mx>
2025-10-22 15:02:15 -0600

    inicio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+    <title>Inicio</title>
+</head>
+<header>
+    <div id="divNavBar">
+        <nav id="navBar">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
+<body>
+    <div id="divTitulo">
+    <h1 id="h1Dulces">
+        Dulces Ricos
+    </h1>
+    </div>
+</body>
+</html>
